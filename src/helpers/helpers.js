ab7cdbcdd6f259e45916d7a3a58b1abd2313766a
Karol <karol.kozer@gmail.com>
2018-06-22 21:09:35 +0200

    add helpers js
    add colors
    add inputLabels obj

---
+++ src/helpers/helpers.js
@@ -0,0 +1,23 @@
+export const colors = {
+	color_blue: '#3b92ff',
+	color_white: '#fff'
+};
+
+export const inputLabels = {
+	name: {
+		name: 'Name',
+		type: 'text'
+	},
+	email: {
+		name: 'Email',
+		type: 'email'
+	},
+	phone: {
+		name: 'Phone number',
+		type: 'number'
+	},
+	password: {
+		name: 'Password',
+		type: 'password'
+	}
+};

6f7e5d8618d213956c72af656095d508b0219471
Karol <karol.kozer@gmail.com>
2018-06-22 21:09:14 +0200

    add Form js
    add html
    add styles material ui
    import helpers: inputLabels, colors

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,87 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import { TextField, Button } from '@material-ui/core';
+import { inputLabels, colors } from '../helpers/helpers';
 
-export default class Form extends React.Component {
+const styles = (theme) => ({
+	label: {
+		fontSize: 18,
+		color: colors.color_blue
+	},
+	input: {
+		fontSize: 16
+	},
+	underline: {
+		'&:after': {
+			borderBottomColor: colors.color_blue
+		}
+	},
+	button: {
+		fontSize: 14,
+		fontWeight: 400,
+		backgroundColor: colors.color_blue,
+		borderRadius: 20,
+		padding: '1rem 5rem',
+		color: colors.color_white,
+		'&:hover': {
+			color: colors.color_blue,
+			backgroundColor: colors.color_white
+		}
+	}
+});
+
+const labels = {
+	name: 'Name',
+	email: 'Email',
+	phone: 'Phone number (+48 or 001)',
+	password: 'Password'
+};
+
+class Form extends React.Component {
+	state = {
+		name: '',
+		email: '',
+		password: ''
+	};
+
 	render() {
+		const { classes } = this.props;
 		return (
 			<form className="form">
 				<h2 className="heading-second">Sign Up</h2>
+
+				<div className="form__content">
+					{Object.values(inputLabels).map((label) => (
+						<TextField
+							key={label.name}
+							required
+							label={label.name}
+							type={label.type}
+							margin="normal"
+							fullWidth={true}
+							InputProps={{
+								classes: {
+									root: classes.input,
+									underline: classes.underline
+								}
+							}}
+							InputLabelProps={{
+								className: classes.label
+							}}
+						/>
+					))}
+				</div>
+				<Button variant="contained" size="large" className={classes.button}>
+					Send
+				</Button>
 			</form>
 		);
 	}
 }
+
+Form.propTypes = {
+	classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Form);

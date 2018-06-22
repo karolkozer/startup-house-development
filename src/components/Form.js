335e1eeab278c2023fbce3fcf39200fd10d5cc71
Karol <karol.kozer@gmail.com>
2018-06-22 19:42:53 +0200

    add Form js

---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Form extends React.Component {
+	render() {
+		return (
+			<form className="form">
+				<h2 className="heading-second">Sign Up</h2>
+			</form>
+		);
+	}
+}

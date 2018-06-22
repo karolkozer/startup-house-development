8deefe8a9d3d277739e5285ddb463a27fe360543
Karol <karol.kozer@gmail.com>
2018-06-22 19:42:44 +0200

    App js
    import Form js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+// Import components
+import Form from './Form';
+
 export default class App extends React.Component {
 	render() {
 		return (
 			<div className="container">
-				<h1>React App</h1>
+				<Form />
 			</div>
 		);
 	}

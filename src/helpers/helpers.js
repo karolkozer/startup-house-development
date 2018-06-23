cc37090e48d380cb6c5e15bf10bf20419ae6c423
Karol <karol.kozer@gmail.com>
2018-06-23 12:39:31 +0200

    helpers js
    remove comments
    add styles
    import color blue

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,50 +1,19 @@
-const colors = {
-	color_blue: '#3b92ff',
-	color_white: '#fff'
-};
-
-export const inputLabels = {
-	name: {
-		name: 'Name',
-		type: 'text'
-	},
-	email: {
-		name: 'Email',
-		type: 'email'
-	},
-	phone: {
-		name: 'Phone number',
-		type: 'text'
-	},
-	password: {
-		name: 'Password',
-		type: 'password'
-	}
-};
+import blue from '@material-ui/core/colors/blue';
 
 export const styles = (theme) => ({
-	label: {
-		fontSize: 18,
-		color: colors.color_blue
-	},
-	input: {
-		fontSize: 16
-	},
-	underline: {
-		'&:after': {
-			borderBottomColor: colors.color_blue
-		}
-	},
 	button: {
-		fontSize: 14,
-		fontWeight: 400,
-		backgroundColor: colors.color_blue,
-		borderRadius: 20,
-		padding: '1rem 5rem',
-		color: colors.color_white,
+		margin: theme.spacing.unit,
+		backgroundColor: blue[700],
 		'&:hover': {
-			color: colors.color_blue,
-			backgroundColor: colors.color_white
-		}
+			backgroundColor: blue[800]
+		},
+		width: 160,
+		borderRadius: 90,
+		fontWeight: 400,
+		letterSpacing: 1
+	},
+	textfield: {
+		color: blue[700],
+		borderColor: blue[700]
 	}
 });

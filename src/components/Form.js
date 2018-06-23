5678e5a8df30ce1b783a175faff7136a4ba27eff
Karol <karol.kozer@gmail.com>
2018-06-23 12:39:14 +0200

    Form js
    remove style and import blue color

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,25 +3,7 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import blue from '@material-ui/core/colors/blue';
-
-const styles = (theme) => ({
-	button: {
-		margin: theme.spacing.unit,
-		backgroundColor: blue[700],
-		'&:hover': {
-			backgroundColor: blue[800]
-		},
-		width: 160,
-		borderRadius: 90,
-		fontWeight: 400,
-		letterSpacing: 1
-	},
-	textfield: {
-		color: blue[700],
-		borderColor: blue[700]
-	}
-});
+import { styles } from '../helpers/helpers';
 
 class Form extends React.Component {
 	context = {

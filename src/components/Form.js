1a01d15984b8c88f77583976a259438ff26d05c7
Karol <karol.kozer@gmail.com>
2018-06-23 12:37:13 +0200

    form js
    add  TextField, Button, blue, add custom style
    add html Textfields and Button

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import { TextField, Button } from '@material-ui/core';
-import { inputLabels, styles } from '../helpers/helpers';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import blue from '@material-ui/core/colors/blue';
+
+const styles = (theme) => ({
+	button: {
+		margin: theme.spacing.unit,
+		backgroundColor: blue[700],
+		'&:hover': {
+			backgroundColor: blue[800]
+		},
+		width: 160,
+		borderRadius: 90,
+		fontWeight: 400,
+		letterSpacing: 1
+	},
+	textfield: {
+		color: blue[700],
+		borderColor: blue[700]
+	}
+});
 
 class Form extends React.Component {
-	state = {
-		name: '',
-		email: '',
-		password: ''
+	context = {
+		classes: PropTypes.object.isRequired
 	};
 
 	render() {
@@ -16,39 +33,55 @@ class Form extends React.Component {
 		return (
 			<form className="form">
 				<h2 className="heading-second">Sign Up</h2>
-
-				<div className="form__content">
-					{Object.values(inputLabels).map((label) => (
-						<TextField
-							autoComplete="disabled"
-							key={label.name}
-							required
-							label={label.name}
-							type={label.type}
-							margin="normal"
-							fullWidth={true}
-							InputProps={{
-								classes: {
-									root: classes.input,
-									underline: classes.underline
-								}
-							}}
-							InputLabelProps={{
-								className: classes.label
-							}}
-						/>
-					))}
+				<div className="form__fields">
+					<TextField
+						id="name"
+						name="name"
+						label="Name"
+						type="text"
+						margin="normal"
+						required
+						fullWidth={true}
+					/>
+					<TextField
+						id="email"
+						name="email"
+						label="Email"
+						type="email"
+						margin="normal"
+						required
+						fullWidth={true}
+					/>
+					<TextField
+						id="phone"
+						name="phone"
+						label="Phone number"
+						type="text"
+						margin="normal"
+						required
+						fullWidth={true}
+					/>
+					<TextField
+						id="password"
+						name="password"
+						label="Password"
+						type="password"
+						margin="normal"
+						required
+						fullWidth={true}
+					/>
 				</div>
-				<Button variant="contained" size="large" className={classes.button}>
+				<Button
+					variant="contained"
+					size="large"
+					color="primary"
+					className={classes.button}
+				>
 					Send
 				</Button>
 			</form>
 		);
 	}
 }
-
-Form.propTypes = {
-	classes: PropTypes.object.isRequired
-};
 
 export default withStyles(styles)(Form);

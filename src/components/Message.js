7b76490578828f0e3206dd10fd1c503d2e5be1dc
Karol <karol.kozer@gmail.com>
2018-06-23 23:24:58 +0200

    Message js
    import icon
    add JSX

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Icon from '@material-ui/core/Icon';
 
 const Message = () => (
 	<div className="message">
-		<p className="message__description">Completed!</p>
+		<Icon style={{ fontSize: 80 }}>done</Icon>
+		<p className="message__description">Successfully completed!</p>
 	</div>
 );
 

540190df7987bbdafeb9af67ba52f94837953ecf
Karol <karol.kozer@gmail.com>
2018-06-23 13:33:31 +0200

    App js
    remove MultiTheme provider

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 // Import components
 import Form from './Form';
@@ -7,13 +6,11 @@ import Form from './Form';
 export default class App extends React.Component {
 	render() {
 		return (
-			<MuiThemeProvider>
-				<div className="container">
-					<main className="main">
-						<Form />
-					</main>
-				</div>
-			</MuiThemeProvider>
+			<div className="container">
+				<main className="main">
+					<Form />
+				</main>
+			</div>
 		);
 	}
 }
